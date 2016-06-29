4b199a69e0c4de9de4e67699851f0edd53c8e961
Damian Palpacelli <damian.palpacelli@gmail.com>
2016-06-29 12:35:45 -0300

    formatted list with options

--- src/client/app/+users/users-list.component.ts
+++ src/client/app/+users/users-list.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { REACTIVE_FORM_DIRECTIVES } from '@angular/forms/index';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {REACTIVE_FORM_DIRECTIVES} from '@angular/forms';
+import {Router} from '@angular/router';
 
-import { UsersService } from './users.service';
+import {UsersService} from './users.service';
 
 /**
  * This class represents the lazy loaded UsersComponent.
@@ -13,25 +14,49 @@ import { UsersService } from './users.service';
   styleUrls: ['users-list.component.css'],
   directives: [REACTIVE_FORM_DIRECTIVES]
 })
-export class UsersListComponent {
+export class UsersListComponent implements OnInit, OnDestroy {
 
-  newName: string;
+  newName:string;
+  private selectedId:number;
+  private sub:any;
 
   /**
    * Creates an instance of the UsersComponent
    *
    * @param usersService
    */
-  constructor(public usersService: UsersService) {}
+  constructor(public usersService:UsersService, private router:Router) {
+  }
+
+  ngOnInit() {
+    this.sub = this.router
+      .routerState
+      .queryParams
+      .subscribe(params => {
+        this.selectedId = +params['id'];
+      });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 
   /**
    * Calls the add method.
    * @return {boolean} false to prevent default form submit behavior to refresh the page.
    */
-  addUser(): boolean {
+  addUser():boolean {
     this.usersService.add(this.newName);
     this.newName = '';
     return false;
+  }
+
+  isSelected(user) {
+    return user.id === this.selectedId;
+  }
+
+  onSelect(user) {
+    this.router.navigate(['/users'], {queryParams: {id: user.id}});
   }
 
 }
