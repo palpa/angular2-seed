1c503e747f4d21f6e45781154aabf5cfe3315d51
Damian Palpacelli <damian.palpacelli@gmail.com>
2016-07-27 21:17:33 -0300

    complete Reparation

--- src/client/app/+reparations/Reparation.ts
+++ src/client/app/+reparations/Reparation.ts
@@ -1,5 +1,15 @@
 export interface Reparation {
   id:number;
+  failure:string;
   deviceSerialNumber:string;
+  deviceTypeId:number;
+  deviceTypeName:string;
+  currentStateName:string;
+  currentResponsibleName:string;
+  currentStateDescriptionId:number;
+  creationDate:Date;
+  completed:boolean;
+  reparationCycleId:number;
+  reparationCycleName:string;
   lastUpdated:Date;
 }
